b35a9528fd607db07ba33c4fdad7447708e3a8ff
DESKTOP-0LVN62V\Modern <askari.zahra94@gmail.com>
2024-09-26 11:34:06 -0500

    make pages dynamic

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,25 +1,35 @@
-import styles from "./blog.module.css";
 import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+import { getPosts } from "@/lib/data";
 
-const BlogPage = () => {
+// FETCH DATA WITH AN API
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const BlogPage = async () => {
+
+  // FETCH DATA WITH AN API
+  const posts = await getData();
+
+  // FETCH DATA WITHOUT AN API
+  // const posts = await getPosts();
+
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard  />
-      </div>
-      <div className={styles.post}>
-        <PostCard  />
-      </div>
-      <div className={styles.post}>
-        <PostCard  />
-      </div>
-      <div className={styles.post}>
-        <PostCard  />
-      </div>
-      
-  
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
